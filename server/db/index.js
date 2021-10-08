f496b1abf0f2fea4ef6131fca5b4bdcb01c87a04
Anshu Patel <anshupatel314@gmail.com>
2021-10-08 11:22:27 -0400

    created models and associations

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,23 @@
-const db = require('./db')
-// require each of your models here...
+const db = require("./db");
+const Album = require("./album");
+const Artist = require("./artist");
+const Song = require("./song");
 
 // ...and give them some nice associations here!
+Album.belongsTo(Artist);
+Artist.hasMany(Album);
+
+Song.belongsTo(Artist);
+Artist.hasMany(Song);
+
+Song.belongsTo(Album);
+Album.hasMany(Song);
 
 module.exports = {
   db,
   // Include your models in your module.exports as well!
   // The seed file expects to find them there!
-}
+  Album,
+  Artist,
+  Song,
+};
